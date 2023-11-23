5ab59e7a6c46b2df2635dab5622f6714b7484495
Adam <adam@xyzdigital.com>
2023-11-23 11:55:56 -0800

    Switch to static deployment and organize rust example

---
+++ examples/rust/Readme.md
@@ -0,0 +1,17 @@
+# Rust WASM Art Example
+
+## Install Rust
+
+## Add wasm target
+
+## Compile wasm
+
+```bash
+make
+```
+
+Wasm output will be written to target/wasm32-unknown-unknown/release/wasm_art_example.wasm
+
+## Try It
+
+Head to [https://wasm-art-main-adamjberg.engram.sh/](https://wasm-art-main-adamjberg.engram.sh) and drag the built wasm file onto the canvas.
--- examples/rust/src/lib.rs
+++ examples/rust/src/lib.rs
@@ -9,24 +9,24 @@ use core::sync::atomic::{AtomicU32, Ordering};
 
 static FRAME: AtomicU32 = AtomicU32::new(0);
 
-const WIDTH: usize = 480;
-const HEIGHT: usize = 640;
+const WIDTH: usize = 600;
+const HEIGHT: usize = 600;
 
 #[no_mangle]
 static mut BUFFER: [u32; WIDTH * HEIGHT] = [0; WIDTH * HEIGHT];
 
 #[no_mangle]
-pub unsafe extern fn go() {
+pub unsafe extern fn draw() {
     render_frame_safe(&mut BUFFER)
 }
 
 unsafe fn render_frame_safe(buffer: &mut [u32; WIDTH * HEIGHT]) {
-    // This line is new:
     let f = FRAME.fetch_add(1, Ordering::Relaxed);
 
     for y in 0..HEIGHT {
         for x in 0..WIDTH {
             *buffer.get_unchecked_mut(y * WIDTH + x) = f.wrapping_add((x ^ y) as u32) | 0xFF_00_00_00;
+            // TODO: Using this significantly increases wasm size
             // buffer[y * WIDTH + x] = f.wrapping_add((x ^ y) as u32) | 0xFF_00_00_00;
         }
     }
